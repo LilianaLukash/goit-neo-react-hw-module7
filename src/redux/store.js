cc0df814647ea9f00e4808a7e03b76fcedaa06d7
Liliana <liliana.ubisoft@gmail.com>
2024-10-19 18:06:22 -0500

    update

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,41 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import contactsReducer from './contactsSlice';
 import filtersReducer from './filtersSlice';
-import {
-  persistStore,
-  persistReducer,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
-
-const contactsPersistConfig = {
-  key: 'contacts',
-  storage,
-  whitelist: ['contacts'], // зберігаємо лише поле contacts
-};
-
-const persistedContactsReducer = persistReducer(contactsPersistConfig, contactsReducer);
-
 
 const store = configureStore({
   reducer: {
-    contacts: persistedContactsReducer, // використовуємо персистований редюсер
+    contacts: contactsReducer,
     filters: filtersReducer,
   },
-  middleware: getDefaultMiddleware =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        // Ігноруємо певні дії для серіалізації
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    }),
 });
-
-export const persistor = persistStore(store);
 
 export default store;
